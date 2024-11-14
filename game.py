884bf41f28e975934d0126f4a9add0fa5e096f07
Victor Lum <vlum8@my.bcit.ca>
2024-11-14 12:54:53 -0800

    Create doctests for make_board

--- game.py
+++ game.py
@@ -10,6 +10,23 @@ def make_board(rows, columns):
     :postcondition: create a game board with the given number of rows and columns
     :return: a dictionary with each key as a tuple with a set of coordinates, and
     each value as a short string description of the difficulty level
+
+    >>> rows_example_one = 1
+    >>> columns_example_one = 1
+    >>> board = make_board(rows_example_one, columns_example_one)
+    >>> board
+    {(0, 0): 'Start (no difficulty level)'}
+    >>> rows_example_two = 5
+    >>> columns_example_two = 5
+    >>> board = make_board(rows_example_two, columns_example_two)
+    >>> board
+    {
+        (0, 0): 'Start (no difficulty level)', (0, 1): 'Easy', (0, 2): 'Easy', (0, 3): 'Easy', (0, 4): 'Medium',
+        (1, 0): 'Easy', (1, 1): 'Easy', (1, 2): 'Easy', (1, 3): 'Medium', (1, 4): 'Hard',
+        (2, 0): 'Easy', (2, 1): 'Easy', (2, 2): 'Medium', (2, 3): 'Medium', (2, 4): 'Hard',
+        (3, 0): 'Medium', (3, 1): 'Medium', (3, 2): 'Medium', (3, 3): 'Hard', (3, 4): 'Very Hard',
+        (4, 0): 'Medium', (4, 1): 'Medium', (4, 2): 'Hard', (4, 3): 'Very hard', (4, 4): 'Final Boss',
+    }
     """
     pass
 
